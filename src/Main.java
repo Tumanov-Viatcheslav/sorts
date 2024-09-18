aaf7d3bf111bdcbbf4b54dea2e07889cf3842797
Tumanov <tslavich@bk.ru>
2024-09-18 13:02:39 +0300

    -Implemented merge sort

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int[] data = {6, 55, 5, 4, 3, 0, 2, 1, 107};
+        Sorts.mergeSort(data);
+        for (int e :
+                data) {
+            System.out.println(e);
+        }
     }
 }
--- src/Sorts.java
+++ src/Sorts.java
@@ -2,6 +2,40 @@ import java.util.Arrays;
 
 public class Sorts {
 
+    private static int[] merge(int[] arr1, int[] arr2) {
+        int[] res = new int[arr1.length + arr2.length];
+        int i1 = 0, i2 = 0, k = 0;
+        while (i1 < arr1.length && i2 < arr2.length) {
+            if (arr1[i1] <= arr2[i2]) {
+                res[k] = arr1[i1];
+                i1++;
+            }
+            else {
+                res[k] = arr2[i2];
+                i2++;
+            }
+            k++;
+        }
+        if (i1 == arr1.length) {
+            System.arraycopy(arr2, i2, res, k, arr2.length - i2);
+        }
+        else {
+            System.arraycopy(arr1, i1, res, k, arr1.length - i1);
+        }
+        return res;
+    }
+
+    public static void mergeSort(int[] arr) {
+        if (arr.length == 1)
+            return;
+        int[] arr1 = new int[arr.length / 2], arr2 = new int[arr.length - arr1.length];
+        System.arraycopy(arr, 0, arr1, 0, arr1.length);
+        System.arraycopy(arr, arr1.length, arr2, 0, arr2.length);
+        mergeSort(arr1);
+        mergeSort(arr2);
+        System.arraycopy(merge(arr1, arr2), 0, arr, 0, arr.length);
+    }
+
     public static void radixSortLSD(int[] arr, int radix) {
         int mask = 1, max = ArrayRoutines.max(arr), indexTmp, tmp;
         int[] indexesInBuckets = new int[radix], tmpArr = new int[arr.length];
