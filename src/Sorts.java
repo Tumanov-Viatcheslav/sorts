364d2f426e6353f51271a1d60f0eecea9099b4d2
TumanovVA <tslavich@bk.ru>
2024-09-18 21:27:16 +0300

    -Fix

--- src/Sorts.java
+++ src/Sorts.java
@@ -75,9 +75,9 @@ public class Sorts {
     }
 
     public static void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] > arr[j])
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1])
                     ArrayRoutines.swapTwoElements(arr, i, j);
             }
         }
@@ -85,10 +85,10 @@ public class Sorts {
 
     public static void bubbleSortF(int[] arr) {
         boolean flag;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             flag = false;
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
                     ArrayRoutines.swapTwoElements(arr, i, j);
                     flag = true;
                 }
